2d2fff0f186426eef6097924790a4c5e2436d679
stephenh0191 <stephenhughes0191@gmail.com>
2020-10-22 20:04:33 +0100

    completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
-Rails.application.routes.draw do
 
-	resources :projects do 
-		resources :tasks do 
-			member do
-				patch :complete
-			end
-		end
-	end
+Rails.application.routes.draw do
+  
+  resources :projects do
+    resources :tasks do
+    	member do 
+    		patch :complete
+    	end
+    end
+  end
+
+  root "projects#index"
 end
+
+	
